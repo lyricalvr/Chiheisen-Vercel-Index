511bdc65a78afe93e430567b555cf9783a38b11b
Sigmund Freud <124454312+lyricalvr@users.noreply.github.com>
2024-07-18 10:51:07 +0530

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/Studied Folder (Protected)']
+  protectedRoutes: ['/Studied Folder (Protected)'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:lyrical_vr@proton.me',
